0771e81904c39f129c7502de04b392e94e920c55
milunax <clemto95@live.fr>
2025-01-29 13:17:06 +0100

    [MOD] Directional Pad System
    Added :
    - DIRECTIONAL_PAD_INFO.cs -> pad direction enum
    - DirectionalPadButton.cs
    - DirectionalPadButtonEditor.cs -> replace DirectionalPadEditor.cs
    
    Modified :
    - DirectionalPad.cs -> new intermediate, receive input and decide to call function of buttons or not

---
+++ Assets/_Project/Runtime/Scripts/Enums/DIRECTIONAL_PAD_INFO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DIRECTIONAL_PAD_INFO
+{
+    NONE = 0,
+    UP = 1,
+    DOWN = 2,
+    LEFT = 3,
+    RIGHT = 4,
+    CONFIRM = 5
+}
