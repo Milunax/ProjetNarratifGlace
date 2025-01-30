5ef7853f8bd312155c71ec7f8a0c29ebd64b3103
Clément THOMAS <clemto95@live.fr>
2025-01-30 23:28:45 +0100

    [MOD] Tests on pipes and pipe game

--- Assets/_Project/Runtime/Scripts/PipesGame/Pipe.cs
+++ Assets/_Project/Runtime/Scripts/PipesGame/Pipe.cs
@@ -6,9 +6,12 @@ public class Pipe : MonoBehaviour
 {
     [SerializeField] private int _id;
     [SerializeField] private bool _isSelectable;
+    [SerializeField] private bool _isFilled;
 
     [SerializeField] private Pipe _previousPipe;
     [SerializeField] private Pipe _nextPipe;
+
+    private int[] _possibleStartRotations = { 0, 90, 180, 270};
 
     #region PROPERTIES
     public int Id { get { return _id; } }
@@ -17,9 +20,19 @@ public class Pipe : MonoBehaviour
     public Pipe NextPipe { get { return _nextPipe; } set { _nextPipe = value; } }
     #endregion
 
+    private void Start()
+    {
+        transform.eulerAngles = new Vector3 (transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, _possibleStartRotations[Random.Range(0, 4)]);
+    }
+
     //CONTINUER A PARTIR DE L'UPDATE A LA SELECTION
     public void UpdatePipe()
     {
 
+    }
+
+    public void RotatePipe()
+    {
+        transform.eulerAngles += new Vector3(0, 0, 90);
     }
 }
--- Assets/_Project/Runtime/Scripts/PipesGame/PipesGame.cs
+++ Assets/_Project/Runtime/Scripts/PipesGame/PipesGame.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PipesGame : MonoBehaviour
@@ -8,15 +9,34 @@ public class PipesGame : MonoBehaviour
     [SerializeField] private List<Pipe> _pipes = new List<Pipe>();
     [SerializeField] private Pipe _selectedPipe;
 
+    [SerializeField] private GameObject _pipesContainer;
+    [SerializeField] private GameObject _pipePrefab;
+    [SerializeField] private float _spawnOffset;
+
+    [SerializeField] private int _gridRow;
+    [SerializeField] private int _gridColumn;
+    [SerializeField] private List<Pipe[]> _pipesGrid = new List<Pipe[]>();
+
     public Pipe SelectedPipe { get { return _selectedPipe; } private set { _selectedPipe = value; /* A FAIRE UPDATE DE LA PIPE SELECTIONNEE */} }
 
     void Start()
     // Start is called before the first frame update
     {
-        //FindObjectOfType<DirectionalPad>().OnKeyPressed += 
+        FindObjectOfType<DirectionalPad>().OnKeyPressed += MoveBetweenPipes;
 
+        for(int i = 0; i < _gridRow; i++)
+        {
+            Pipe[] newRow = new Pipe[_gridColumn];
+            _pipesGrid.Add(newRow);
 
-        _pipes.AddRange(GetComponentsInChildren<Pipe>());
+            for(int j = 0; j < _gridColumn; j++)
+            {
+                GameObject createdObject = Instantiate(_pipePrefab, new Vector3(transform.position.x + _spawnOffset * j, transform.position.y + _spawnOffset * i, transform.position.z),Quaternion.identity, _pipesContainer.transform);
+                _pipesGrid[i][j] = createdObject.GetComponent<Pipe>(); 
+            }
+        }
+
+        /*_pipes.AddRange(GetComponentsInChildren<Pipe>());
         _pipes = _pipes.OrderBy(e => e.Id).ToList();
 
         for(int i = 0; i < _pipes.Count; i++)
@@ -37,14 +57,12 @@ public class PipesGame : MonoBehaviour
 
             _pipes[i].NextPipe = _pipes[i+1];
             _pipes[i].PreviousPipe = _pipes[i-1];
-        }
+        }*/
     }
+    
 
-
-
-    // Update is called once per frame
-    void Update()
+    private void MoveBetweenPipes(DIRECTIONAL_PAD_INFO directionInfo)
     {
-        
+
     }
 }
