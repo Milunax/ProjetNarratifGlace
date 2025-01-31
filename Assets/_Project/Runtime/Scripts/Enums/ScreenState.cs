049d408c6f63b8694eb480d438715f730932403f
Nirgigo <titouan@dupuisland.net>
2025-01-31 11:49:58 +0100

    [ADD] ScreenDisplay Manager + [MOD] FileExplorer
    Added:
    + ScreenDisplay.cs
    + ScreenState.cs
    + Debug/IGFileExplorer -> Debugs assets
    
    Modified:
    ~ FileExplorerBase.cs -> BrowsDataRefresh() (fixes)
    ~ FileExplorerBase.cs -> TotalDataLines()
    ~ FileExplorerBase.cs -> CreateTextLine()
    ~ FileExplorerBase.cs -> SetupTextDisplayValues()
    ~ FileExplorerBase.cs -> SetDisplayLines()
    ~ FileExplorerBase.cs -> UpdateDisplayLine()

---
+++ Assets/_Project/Runtime/Scripts/Enums/ScreenState.cs
@@ -0,0 +1,35 @@
+public enum SCREEN_ACTIVE
+{
+    SHUTDOWN = 0,
+    BLACK_SCREEN = 1,
+    MAP = 10,
+    FILE_EXPLORER = 11,
+    DIALOGUE = 12
+}
+
+public enum MAP_ACTIVE
+{
+    CLOSED = 0,
+    MAP = 10,
+    DOOR_SIMON = 11,
+    SWITCH = 12,
+    PIPES = 13,
+    NUMBERS_CODE = 14,
+    NUMBERS_LOGIC = 15
+}
+
+public enum FILE_EXPLORER_ACTIVE
+{
+    CLOSED = 0,
+    FILE_EXPLORER = 10,
+    TEXT = 11,
+    AUDIO = 12
+}
+
+public enum DIALOGUE_ACTIVE
+{
+    CLOSED = 0,
+    DIALOGUE_NO_INTERACTIVE = 10,
+    DIALOGUE_INTERACTIVE = 11
+
+}
--- Assets/_Project/Runtime/Scripts/SO/FileExplorer/FileExplorerDataSO.cs
+++ Assets/_Project/Runtime/Scripts/SO/FileExplorer/FileExplorerDataSO.cs
@@ -8,4 +8,6 @@ public class FileExplorerDataSO : ScriptableObject
     [Header("Accessibility")]
     public PROGRESSION isAccessible = new PROGRESSION(0, 0, 0);
     public PROGRESSION isVisible = new PROGRESSION(0, 0, 0);
+    public bool manualLock;
+    public bool manualIsUnlock;
 }
