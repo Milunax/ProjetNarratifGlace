01311758bec0445a6f48fb9250541e9ff847e078
riufix <enguerrand.cousin@gmail.com>
2025-02-05 15:15:28 +0100

    [MOD]: circuitbreaker + wheel
    Modified:
    - switchs selection
    - LED feedback
    - wheel behaviour
    Added:
    - wave target on the scene

--- Assets/_Project/Runtime/Scripts/Switchs/CircuitBreaker.cs
+++ Assets/_Project/Runtime/Scripts/Switchs/CircuitBreaker.cs
@@ -16,13 +16,14 @@ public class CircuitBreaker : MonoBehaviour
     [SerializeField] private List<Switchs> _switchs = new List<Switchs>();
     [SerializeField] private List<Image> _ledList = new List<Image>();
     [SerializeField] private Switchs _selectedSwitch;
-    [SerializeField] private Image _SelectedLED;
+    [SerializeField] private Image _selectedLED;
 
     [Header("Parameters")]
-    [SerializeField] Color _SelectedColor;
+    [SerializeField] Color _selectedColor;
+    [SerializeField] Color _defaultColor;
 
     public Switchs selectedSwitch { get {return _selectedSwitch;} private set {_selectedSwitch = value;} }
-    public Image selectedLED { get {return _SelectedLED;} private set {_SelectedLED = value;} }
+    public Image selectedLED { get {return _selectedLED;} private set {_selectedLED = value;} }
 
     void OnEnable()
     {
@@ -36,41 +37,43 @@ public class CircuitBreaker : MonoBehaviour
 
     private void MoveBetweenSwitchs(DIRECTIONAL_PAD_INFO switchInfo)
     {
+        int idPreviousSwitch = _switchs.IndexOf(selectedSwitch);
+        int idNextSwitch = _switchs.IndexOf(selectedSwitch);
+        int idPreviousLED = _ledList.IndexOf(selectedLED);
+        int idNextLED = _ledList.IndexOf(selectedLED);
+
         switch (switchInfo)
         {
             case DIRECTIONAL_PAD_INFO.LEFT:
-                Debug.Log("Left");
-                int idPreviousSwitch = _switchs.IndexOf(selectedSwitch);
-                int idPreviousLED = _ledList.IndexOf(selectedLED);
-                if (idPreviousSwitch == 0)
+                int curentSwitch = idPreviousSwitch - 1;
+                curentSwitch = curentSwitch >= _switchs.Count ? 0 : curentSwitch < 0 ? _switchs.Count - 1 : curentSwitch;
+                selectedSwitch = _switchs[curentSwitch];
+                //Debug.Log(idPreviousSwitch);
+
+                int currentLED = idPreviousLED - 1;
+                currentLED = currentLED >= _ledList.Count ? 0 : currentLED < 0 ? _ledList.Count - 1 : currentLED;
+                selectedLED = _ledList[currentLED];
+                //Debug.Log(idPreviousLED);
+                if (selectedLED == _ledList[idPreviousLED])
                 {
-                    selectedSwitch = _switchs[_switchs.Count - 1];
-                    selectedLED = _ledList[_ledList.Count - 1];
+                    _ledList[idPreviousLED].DOColor(_selectedColor, .25f);
                 }
-                else
-                {
-                    selectedSwitch = _switchs[idPreviousSwitch - 1];
-                    selectedLED = _ledList[idPreviousLED - 1];
-                    Debug.Log(idPreviousSwitch);
-                }
+                else if(selectedLED != _ledList[idPreviousLED])
+                    _ledList[idPreviousLED].DOColor(_defaultColor, .25f);
 
                 break;
             case DIRECTIONAL_PAD_INFO.RIGHT:
-                Debug.Log("Right");
-                int idNextSwitch = _switchs.IndexOf(selectedSwitch);
-                int idNextLED = _ledList.IndexOf(selectedLED);
-                if(idNextSwitch == _switchs.Count - 1)
-                {
-                    selectedSwitch = _switchs[0];
-                    selectedLED = _ledList[0];
-                }
-                else
-                {
-                    selectedSwitch = _switchs[idNextSwitch - 1];
-                    selectedLED = _ledList[idNextLED - 1];
-                    _SelectedLED.DOColor(_SelectedColor, .25f);
-                    Debug.Log(idNextLED);
-                }
+                idNextSwitch++;
+                idNextSwitch = idNextSwitch >= _switchs.Count ? 0 : idNextSwitch < 0 ? _switchs.Count - 1 : idNextSwitch;
+                selectedSwitch = _switchs[idNextSwitch];
+                //Debug.Log(idNextSwitch);
+
+                idNextLED++;
+                idNextLED = idNextLED >= _ledList.Count ? 0 : idNextLED < 0 ? _ledList.Count - 1 : idNextLED;
+                _selectedLED.DOColor(_selectedColor, .25f);
+                selectedLED = _ledList[idNextLED];
+                //Debug.Log(idNextLED);
+                
                 break;
         }
     }
--- Assets/_Project/Runtime/Scripts/Wheel/WheelBehaviour.cs
+++ Assets/_Project/Runtime/Scripts/Wheel/WheelBehaviour.cs
@@ -12,21 +12,22 @@ using UnityEngine.UI;
 public class WheelBehaviour : MonoBehaviour
 {
     [Header("References")]
-    private Shader _waveMaterial;
     [SerializeField] GameObject _waveImage;
-    CanvasRenderer _canvaRenderer;
+    [SerializeField] GameObject _waveTarget;
+    CanvasRenderer _canvaRendererWave;
+    CanvasRenderer _canvaRendererWaveTarget;
 
     [Header("Parameters")]
     [SerializeField] float _minWheelVal = 0.0f;
     [SerializeField] float _maxWheelVal = 100.0f;
     [SerializeField] float _multiplyFactor = 1f;
-    [SerializeField] float _valToObtain = 1f;
     [SerializeField] TMP_Text _scrolltext;
 
     private Coroutine _UpdateWheel;
     private bool _isActive;
 
     private float _saveStartValue;
+    private float _saveWaveTargetValue;
     public float GetSaveStartValue {  get => _saveStartValue;}
 
 
@@ -37,8 +38,8 @@ public class WheelBehaviour : MonoBehaviour
 
         GameManager.Instance.SetWheelMinMax(_minWheelVal, _maxWheelVal);
         _isActive = GameManager.Instance.GetSetWaveValidity;
-        var shader = Shader.Find("Custom/SineWave");
-        _canvaRenderer = _waveImage.GetComponent<CanvasRenderer>();
+        _canvaRendererWave = _waveImage.GetComponent<CanvasRenderer>();
+        _canvaRendererWaveTarget = _waveTarget.GetComponent<CanvasRenderer>();
         //_waveImage.SetActive(false);
         StartCoroutine(GetWaveMaterial());
     }
@@ -66,7 +67,6 @@ public class WheelBehaviour : MonoBehaviour
         {
             _isActive = false;
             StopCoroutine(_UpdateWheel);
-            
             _waveImage.SetActive(false);
         }
     }
@@ -74,14 +74,14 @@ public class WheelBehaviour : MonoBehaviour
     IEnumerator UpdateWheel()
     {
         float startValue = GameManager.Instance.GetSetWheelValue;
-
         while (true)
         {
             _scrolltext.text = GameManager.Instance.GetSetWheelValue.ToString();
             GameManager.Instance.GetSetWheelValue = startValue + (GameManager.playerInputs.GetSlideDeltaV.y * _multiplyFactor);
-            _canvaRenderer.GetMaterial().SetFloat("_Wave1Frequency", GameManager.Instance.GetSetWheelValue/_maxWheelVal * 10);
-            print(_canvaRenderer.GetMaterial().GetFloat("_Wave1Frequency"));
-            if (_valToObtain == GameManager.Instance.GetSetWheelValue)
+            _canvaRendererWave.GetComponent<Image>().material.SetFloat("_Wave1Frequency", GameManager.Instance.GetSetWheelValue/_maxWheelVal * 10);
+            _canvaRendererWaveTarget.GetComponent<Image>().material.SetFloat("_Wave1Frequency", GameManager.Instance.GetSetWheelValue/_maxWheelVal * 10);
+            //print(_canvaRendererWave.GetMaterial().GetFloat("_Wave1Frequency"));
+            if (_saveWaveTargetValue == GameManager.Instance.GetSetWheelValue)
             {
                 Debug.Log("Wave game Win");
             }
@@ -93,13 +93,14 @@ public class WheelBehaviour : MonoBehaviour
     {
         do
         {
-            if (_canvaRenderer.GetMaterial() != null)
+            if (_canvaRendererWave.GetComponent<Image>().material != null && _canvaRendererWaveTarget.GetComponent<Image>().material != null)
             {
-                Debug.Log("not null");
-                _saveStartValue = _canvaRenderer.GetMaterial().GetFloat("_Wave1Frequency");
+                //Debug.Log("not null");
+                _saveStartValue = _canvaRendererWave.GetComponent<Image>().material.GetFloat("_Wave1Frequency");
+                _saveWaveTargetValue = _canvaRendererWaveTarget.GetComponent<Image>().material.GetFloat("_Wave1Frequency");
             }
 
             yield return new WaitForFixedUpdate();
-        } while (_canvaRenderer.GetMaterial() == null);
+        } while (_canvaRendererWave.GetMaterial() == null);
     }
 }
